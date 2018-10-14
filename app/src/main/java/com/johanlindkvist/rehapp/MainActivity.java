1e851b8cbf74513c2a7fd245d64969891aaebaa5
Johan Lindkvist <johanlindkvist89@gmail.com>
2018-10-14 15:14:50 +0200

    Initial commit

---
+++ app/src/main/java/com/johanlindkvist/rehapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.johanlindkvist.rehapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
